54d8ac045e077021c72e477f1f4e9b424b337600
o00O00o <gaoyibo157@gmail.com>
2020-04-14 19:53:37 +0800

    first update

---
+++ src/main/java/percolation/App.java
@@ -0,0 +1,10 @@
+package percolation;
+import edu.princeton.cs.algs4.RandomSeq;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

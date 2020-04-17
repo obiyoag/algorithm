daa1a43ea8d2df9bbef0bee511338a1bff9a8a56
o00O00o <gaoyibo157@gmail.com>
2020-04-17 14:44:05 +0800

    updata

---
+++ src/main/java/assignment/assignment1/Percolation.java
@@ -0,0 +1,34 @@
+package assignment.assignment1;
+
+import edu.princeton.cs.algs4.RandomSeq;
+
+// public class Percolation {
+//     // Create a n-by-n grid with all sites initally blocked
+//     public Percolation(int n) {
+
+//     }
+
+//     public void open(int row, int col) {
+
+//     }
+
+//     public boolean isOpen(int row, int col) {
+
+//     }
+
+//     public boolean isFull(int row, int col) {
+
+//     }
+
+//     public int numberOfOpenSites() {
+
+//     }
+
+//     public boolean percolates() {
+
+//     }
+
+//     public static void main(String[] args) {
+
+//     }
+// }
---
+++ src/main/java/chapter/chapter1/section1/README.md
@@ -0,0 +1,23 @@
+# 1.1 Basic Programming Model
+
++ 我们把描述和实现算法所用到的语言特性、软件库和操作系统特性总称为
+
++ 本书采用Java语言描述算法，这种方式的坏处是不好把算法的思想与具体的代码描述分离开，受制于具体编程语言
+
++ 还介绍了一些Java的基础语法特性特性和书中实现算法时会使用的API
+
+## Exercise
+
++ [EX03](EX03.java)
+
++ [EX05](EX05.java)
+
++ [EX06](EX06.java)
+
++ [EX07](EX07.java)
+
++ [EX08](EX08.java)
+
++ [EX09](EX09.java)
+
++ [EX11](EX11.java)
